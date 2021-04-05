a739fc7838a351c078c27d4f8843674579cd7770
CodeLikeAlexito <ivanovalexkav@gmail.com>
2021-04-05 22:02:55 +0300

    initial commit

---
+++ Payment.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LabOne
+{
+    public class Payment
+    {
+
+        public List<int> PaymentNums { get; set; }
+
+        public Payment()
+        {
+            PaymentNums = new List<int>();
+            //FillPaymentNums();
+            ReadFromDate(DateTime.Now);
+        }
+
+        private void FillPaymentNums()
+        {
+            using (IDbConnection connection = new SqlConnection(Properties.Settings.Default.DbConnect))
+            {
+                string sqlquery =
+                @"SELECT payment_no FROM payment ";
+                IDbCommand command = new SqlCommand();
+                command.Connection = connection;
+                connection.Open();
+                command.CommandText = sqlquery;
+                IDataReader reader = command.ExecuteReader();
+                bool notEndOfResult;
+                notEndOfResult = reader.Read();
+                while (notEndOfResult)
+                {
+                    int s = reader.GetInt32(0);//reader.GetString(0);
+                    PaymentNums.Add(s);
+                    notEndOfResult = reader.Read();
+                }
+            }
+        }
+
+        // Read rows based on eneterd date
+        private void ReadFromDate(DateTime date)
+        {
+            using (IDbConnection connection = new SqlConnection(Properties.Settings.Default.DbConnect))
+            {
+                string sqlquery = @"select * from [dbo].[payment] where payment_dt > " + date;
+                IDbCommand command = new SqlCommand();
+                command.Connection = connection;
+                connection.Open();
+                command.CommandText = sqlquery;
+                IDataReader reader = command.ExecuteReader();
+                bool notEndOfResult;
+                notEndOfResult = reader.Read();
+                while (notEndOfResult)
+                {
+                    int s = reader.GetInt32(0);//reader.GetString(0);
+                    PaymentNums.Add(s);
+                    notEndOfResult = reader.Read();
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LabOne
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Payment payment = new Payment();
+            foreach (var item in payment.PaymentNums)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
